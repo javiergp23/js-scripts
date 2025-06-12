6828887ac9cff94ec69fbfe49b8b9524e45070c4
javiergp23 <javgon2306@gmail.com>
2025-06-12 10:40:18 -0300

    filter

--- advanced-structures.js
+++ advanced-structures.js
@@ -9,3 +9,14 @@
 let numbers = [1,2,3,4,5];
 
 numbers.forEach(element  => console.log(element))
+
+//map
+
+let double = numbers.map(element => element * 2)
+
+console.log(double)
+
+//filter
+
+let filtered = numbers.filter(element => element % 2 === 0)
+console.log(filtered)
