740a95a3faf66a1185dd00f1e5c5944872548fcc
javiergp23 <javgon2306@gmail.com>
2025-06-24 15:48:15 -0300

    add Singleton

--- advanced-objects.js
+++ advanced-objects.js
@@ -13,3 +13,21 @@ class Person {
 
 const persona_1 = new Person("Eduardo", 30);
 persona_1.greet()
+
+//Singleton
+
+class Session{
+    constructor(name){
+        if(Session.instance){
+            return Session.instance;
+        }
+        Session.instance = this;
+        this.name = name;
+    }
+}
+
+const session1 = new Session("Eduardo");
+const session2 = new Session();
+
+console.log(session1)
+console.log(session2)
